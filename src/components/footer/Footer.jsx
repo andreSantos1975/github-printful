7358169a273db3ae3255cdb88b5609d88e90aa0b
andreSantos1975 <andredev1975@gmail.com>
2023-08-06 19:44:52 -0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='top'>
+        <div className='item'>
+          <h1>Categoria</h1>
+          <span>Mulher</span>
+          <span>Home</span>
+          <span>Bonés</span>
+          <span>Acessórios</span>
+          <span>Novos Lançamentos</span>
+        </div>
+        <div className='item'>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Páginas</span>
+          <span>Lojas</span>
+          <span>Compare</span>
+          <span>Cokies</span>
+        </div>
+        <div className='item'>
+          <h1>Sobre</h1>
+          <span>O próprio cliente é muito dinheiro, mas eu não sei
+            Da mesma forma, eles acontecem ao mesmo tempo que com trabalho de parto e dor. O cliente é muito inteligente.</span>
+        </div>
+        <div className='item'>
+          <h1>Contatos</h1>
+          <span>Rua Major Floriano Peixoto Fortaleza capital CE.</span>
+        </div>
+      </div>
+      <div className='botton'>
+        <div className='left'>
+          <span className='logo'>Melzinha</span>
+          <span className='copyright'>&copy; {new Date().getFullYear()} copyright</span>
+        </div>
+        <div className='right'>
+          <img src="../img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
