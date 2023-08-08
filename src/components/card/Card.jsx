bab70e6e055d7cc9579b005b98bf6b912db2a8d8
andreSantos1975 <andredev1975@gmail.com>
2023-08-08 16:53:13 -0300

    card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,26 +2,26 @@ import React, { useState } from 'react';
 import './Card.scss';
 import { Link } from 'react-router-dom';
 
-export const Card = ({item}) => {
+export const Card = ({ item }) => {
   const [showSecondImage, setShowSecondImage] = useState(false);
 
   return (
-    <Link
-      to={`/product/${item.id}`}
-      onMouseEnter={() => setShowSecondImage(true)}
-      onMouseLeave={() => setShowSecondImage(false)}
-    >
-      <div className='card'>
-        <div className='image'>
+    <div className='card'>
+      <div className='image'>
+        <Link
+          to={`/product/${item.id}`}
+          onMouseEnter={() => setShowSecondImage(true)}
+          onMouseLeave={() => setShowSecondImage(false)}
+        >
           <img src={item.image} alt="" className='mainImg' />
           {item.image2 && showSecondImage && <img src={item.image2} alt="" className='secondImg' />}
-        </div>
-        <h2>{item.title}</h2>
-        <div className='price'>
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.price}</h3>
-        </div>
+        </Link>
       </div>
-    </Link>
+      <h2>{item.title}</h2>
+      <div className='price'>
+        <h3>${item.oldPrice}</h3>
+        <h3>${item.price}</h3>
+      </div>
+    </div>
   );
 };
