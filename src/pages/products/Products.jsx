e04ad81dd42fd5e3586998ddaa6a4e60d9c17376
andreSantos1975 <andredev1975@gmail.com>
2023-08-10 14:27:04 -0300

    list

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Products.scss';
+import image7 from './image/dog7.jpg';
+import { List } from '../../components/list/List';
+import { useParams } from 'react-router-dom';
 
 export const Products = () => {
+
+  const cardId = parseInt(useParams().id)
+  const [maxPrice, setMaxPrice] = useState(1000)
+  const [sort, setSort] = useState(null)
+
+  
   return (
     <div className='products'>
       <div className='left'>
         <div className='filterItem'>
-          <h2>Produdos Categoria</h2>
+          <h2>Produdos por Categoria</h2>
           <div className="inputItem">
             <input type='checkbox' id='1' value={1}/>
             <label htmlFor='1'>Shoes</label>
@@ -24,23 +33,26 @@ export const Products = () => {
           <h2>Filtrar por preço</h2>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" min={0} max={100} />
-            <span>100</span>
+            <input type="range" min={0} max={1000} onClick={(e) =>setMaxPrice(e.target.value)} />
+            <span>{maxPrice}</span>
           </div>
         </div>
         <div className='filterItem'>
           <h2>Ordenar por</h2>
           <div className="inputItem">
-            <input type="radio" id='asc' value='asc' name='price'/>
+            <input type="radio" id='asc' value='asc' name='price' onClick={e =>setSort("asc")}/>
             <label htmlFor="asc">Price (Lowest first)</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id='desc' value='desc' name='price'/>
-            <label htmlFor="desc">Price (Hist first)</label>
+            <input type="radio" id='desc' value='desc' name='price'  onClick={e =>setSort("desc")}/>
+            <label htmlFor="desc">Price (Hight first)</label>
           </div>
         </div>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <img className='catImg' src={image7} alt="" />
+        <List cardId={cardId} maxPrice={maxPrice} sort={sort}/>
+      </div>
     </div>
   )
 }
