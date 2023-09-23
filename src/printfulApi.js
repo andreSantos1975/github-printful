d19a286ff40c894692b2d03d25a751e8115eea56
andreSantos1975 <andredev1975@gmail.com>
2023-09-23 20:23:14 -0300

    first commit

---
+++ src/printfulApi.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const getPrintfulData = async (id) => {
+  try {
+    // Fazer a chamada POST para o seu servidor intermediário com o "id"
+    const response = await axios.post('http://localhost:3001/printful/stores', { productIds: [id] }); 
+
+    // Obter os dados do produto a partir da resposta do servidor intermediário
+    const productData = response.data;
+    console.log('productData getPrintfulData:', productData); // ----------------------------------------------log
+
+    return productData; // Retorna os dados do produto e suas variantes
+  } catch (error) {
+    console.error('Erro ao buscar informações do produto:', error);
+    throw error;
+  }
+};
+
+export default getPrintfulData;
