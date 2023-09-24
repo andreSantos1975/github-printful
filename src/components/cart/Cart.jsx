3a5005a858da65f987c29c11640201b9122ef338
andreSantos1975 <andredev1975@gmail.com>
2023-09-24 15:50:19 -0300

    update product

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -41,7 +41,7 @@ export const Cart = () => {
             <h1>Produtos no seu Carrinho</h1>
             {products?.map(item => (
                 <div className="item" key={item.id}>
-                    <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+                    <img src={item.img} alt="" />
                     <div className="details">
                         <h1>{item.title}</h1>
                         <p>{item.desc?.substring(0, 100)}</p>
