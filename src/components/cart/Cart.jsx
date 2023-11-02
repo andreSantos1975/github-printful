38811025d0ab2ba59ff61b63eee7f6dcaf09a59b
andreSantos1975 <andredev1975@gmail.com>
2023-11-02 11:28:18 -0300

    form

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -11,8 +11,14 @@ export const Cart = () => {
     const products = useSelector(state => state.cart.products);
     const dispatch = useDispatch();
 
-    const [address, setAddress] = useState("");
-    const [city, setCity] = useState("");
+    const [recipient, setRecipient] = useState({
+        name: "",
+        address: "",
+        city: "",
+        state_code: "",
+        country_code: "",
+        zip: "",
+    });
 
 
     const totalPrice = () => {
@@ -28,19 +34,24 @@ export const Cart = () => {
             const res = await axios.post("http://localhost:3001/checkout", {
                 products,
                 recipient: {
-                    address,
-                    city,
+                    name: recipient.name,
+                    address: recipient.address,
+                    city: recipient.city,
+                    state_code: recipient.state_code,
+                    country_code: recipient.country_code,
+                    zip: recipient.zip
                 },
                 success_url: process.env.CLIENT_URL + "?success=true",
                 cancel_url: process.env.CLIENT_URL + "?success=false",
             });
-    
+
+
             window.location.href = res.data.checkoutUrl; // Redirecionar para a página de checkout do Stripe
         } catch (err) {
             console.error(err);
         }
     }
-    
+
 
     // Estado para controlar se o modal está aberto
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -49,6 +60,11 @@ export const Cart = () => {
     const toggleModal = () => {
         setIsModalOpen(!isModalOpen);
     }
+
+    const handleRecipientChange = (e) => {
+        const { name, value } = e.target;
+        setRecipient({ ...recipient, [name]: value });
+    };
 
     return (
         <div className='cart'>
@@ -73,22 +89,40 @@ export const Cart = () => {
             {/* Modal de formulário */}
             {isModalOpen && (
                 <div className="modal">
+
                     <form>
-                        {/* Adicione campos para o endereço de entrega */}
                         <h3>Endereço de Entrega:</h3>
                         <div className="form-group">
-                            <label>Address:</label>
-                            <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
+                            <label>Nome:</label>
+                            <input type="text" name="name" value={recipient.name} onChange={handleRecipientChange} />
                         </div>
                         <div className="form-group">
-                            <label>City:</label>
-                            <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+                            <label>Endereço:</label>
+                            <input type="text" name="address" value={recipient.address} onChange={handleRecipientChange} />
                         </div>
-
-                        {/* Outros campos e botões aqui */}
+                        <div className="form-group">
+                            <label>Cidade:</label>
+                            <input type="text" name="city" value={recipient.city} onChange={handleRecipientChange} />
+                        </div>
+                        <div className="form-group">
+                            <label>Estado:</label>
+                            <input type="text" name="state_code" value={recipient.state_code} onChange={handleRecipientChange} />
+                        </div>
+                        <div className="form-group">
+                            <label>País:</label>
+                            <input type="text" name="country_code" value={recipient.country_code} onChange={handleRecipientChange} />
+                        </div>
+                        <div className="form-group">
+                            <label>CEP:</label>
+                            <input type="text" name="zip" value={recipient.zip} onChange={handleRecipientChange} />
+                        </div>
                     </form>
 
+
+
+
                 </div>
+
             )}
             <button onClick={handlePayment}>PROCESSO DO CHECKOUT</button>
             <span className='reset' onClick={() => dispatch(resetCart())}>Reset Cart</span>
