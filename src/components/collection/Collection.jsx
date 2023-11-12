bf4e690fbb7670dffcaf3454170b25fd45d0c32e
andreSantos1975 <andredev1975@gmail.com>
2023-11-12 09:10:35 -0300

    home

--- src/components/collection/Collection.jsx
+++ src/components/collection/Collection.jsx
@@ -17,9 +17,9 @@ export const Collection = ({ printful }) => {
         console.log(featuredProducts);//--------printfulData FeaturedProducts-----------------------------------log
 
         return (
-            <div className='featuredProducts'>
+            <div className='collection'>
                 <div className='top'>
-                    <h1>Nova Coleção</h1>
+                 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas.</p>
                 </div>
                 <div className='bottom'>
@@ -30,7 +30,7 @@ export const Collection = ({ printful }) => {
     } else {
         // Lidar com o caso em que featuredProducts.sync_variants não é uma matriz ou está vazio
         return (
-            <div className='featuredProducts'>
+            <div className='collection'>
                 <div className='top'>
                     <h1> coleção</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas.</p>
